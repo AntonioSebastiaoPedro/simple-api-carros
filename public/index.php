aed2e19d555a8c968953a87c2e6132bbe3058a5e
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2024-03-02 20:17:37 +0100

    Configurar o cors

--- public/index.php
+++ public/index.php
@@ -1,16 +1,27 @@
 <?php 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Controllers\{CarController};
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
-    $response->getBody()->write('Hello, World');
-    $response->withStatus(200);
 
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', [CarController::class, 'getAll']);
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->run();
